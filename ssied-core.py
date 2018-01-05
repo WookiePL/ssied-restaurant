d3cf4aa617140122d67b88477180f0b4ff92df2a
Mateusz <mateusz.pindel@onet.pl>
2018-01-05 21:52:42 +0100

    pierwszy wykres :D

--- ssied-core.py
+++ ssied-core.py
@@ -1,12 +1,16 @@
 import pandas as pd
+from matplotlib import pyplot
 
 date_info = pd.read_csv("dane/date_info.csv")
 air_reserve = pd.read_csv("dane/air_reserve.csv")
 air_store_info = pd.read_csv("dane/air_store_info.csv")
-air_visit_data = pd.read_csv("dane/air_visit_data.csv")
+air_visit_data = pd.read_csv("dane/air_visit_data.csv", index_col=1, parse_dates=[1])
 hpg_reserve = pd.read_csv("dane/hpg_reserve.csv")
 hpg_store_info = pd.read_csv("dane/hpg_store_info.csv")
 sample_submission = pd.read_csv("dane/sample_submission.csv")
 store_id_relation = pd.read_csv("dane/store_id_relation.csv")
 
-print(sample_submission)
+print(air_visit_data)
+
+air_visit_data['visitors'].plot()
+pyplot.show()
